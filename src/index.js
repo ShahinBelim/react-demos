902dc279f24c0523bb289dd665bb5625874a5939
Shahin Belim <sbelim@codal.com>
2018-11-23 12:56:27 +0530

    react demo covering following concepts   - Creating a simple React component   - Rendering Variables in React components   - Iterating & Rendering with Loops in React components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './bootstrap.min.css';
+class Hello extends React.Component {
+  render() {
+    let targetOfGreeting = "Haters!"
+    let styleClass = 'text-success';
+    return <div className="container">
+      <h3 className="text-center">Hello,
+          <span className={styleClass}>{targetOfGreeting}!</span>
+      </h3>
+    </div>;
+  }
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class UserList extends React.Component {
+  renderHello() {
+    return <Hello />
+  }
+  render() {
+    var list = [
+      'Cras justo odio',
+      'Dapibus ac facilisis in',
+      'Morbi leo risus',
+      'Porta ac consectetur ac',
+      'Vestibulum at eros'
+    ]
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    var listHTML = list.map((value, index) => {
+      return <li className="list-group-item" key={index}>{value}</li>;
+    })
+
+    return (
+      <main role="main">
+        <div className="jumbotron custom">
+          {this.renderHello()}
+          <div className="container">
+            <div className="row text-center">
+              <ul className="list-group custom-list"> {listHTML} </ul>
+            </div>
+          </div>
+        </div>
+      </main>
+    )
+
+  }
+}
+
+ReactDOM.render(<UserList />, document.getElementById('root'));
+
