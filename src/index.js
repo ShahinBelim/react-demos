e55ab910c94d0ff898cb0d5201f8c85f0cf5f59c
Shahin Belim <sbelim@codal.com>
2018-11-26 15:11:36 +0530

    State and Lifecycle

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './bootstrap.min.css';
+
+// Hello Component
 class Hello extends React.Component {
   render() {
     let targetOfGreeting = "Haters!"
@@ -14,6 +16,7 @@ class Hello extends React.Component {
   }
 }
 
+// UserList Component
 class UserList extends React.Component {
   renderHello() {
     return <Hello />
@@ -37,15 +40,81 @@ class UserList extends React.Component {
           {this.renderHello()}
           <div className="container">
             <div className="row text-center">
-              <ul className="list-group custom-list"> {listHTML} </ul>
+              <ul className="list-group custom-list">
+                {listHTML}
+                {/* <li className="list-group custom-list"> <Water /> </li> */}
+                <li className="list-group custom-list"> <Clock /> </li>
+              </ul>
             </div>
           </div>
         </div>
       </main>
     )
-
   }
 }
 
-ReactDOM.render(<UserList />, document.getElementById('root'));
+// Water Component
+class Water extends React.Component {
+  getInitialState() {
+    return {
+      currntTemp: 10
+    };
+  }
+  render() {
+    var stateOfWater;
+    if (this.state.currntTemp <= 32) {
+      stateOfWater = 'Solid';
+    } else if (this.state.currntTemp >= 212) {
+      stateOfWater = 'Gas';
+    } else {
+      stateOfWater = 'Liquid';
+    }
+    return (
+      <div>
+        <p>{this.state.currntTemp}F, water is considered to be a {stateOfWater} state of matter </p>
+      </div>
+    );
+  }
+}
 
+// Clock
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() }
+  }
+
+  // Method runs after the component output has been rendered to the DOM. This is a good place to set up a timer:
+  componentDidMount() {
+    this.timerId = setInterval(() => {
+      this.tick();
+    }, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  tick() {
+    //this.state.date = new Date(); // Don't use this :  Optimized components might not re-render if you do
+    // Rerender component if property changes
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    //console.log("--- call render ---");
+    return (
+      <div>
+        <h3> Current Time : {this.state.date.toLocaleTimeString()} </h3>
+      </div>
+    )
+  }
+}
+
+function createDom() {
+  ReactDOM.render(<UserList />, document.getElementById('root'))
+}
+
+setInterval(createDom, 1000)
