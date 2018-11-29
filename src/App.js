b56dd31a7c0f0e319bfafd7073b077061c5e7b87
Shahin Belim <sbelim@codal.com>
2018-11-29 15:20:42 +0530

    Component lazy load or code-splitting

--- src/App.js
+++ src/App.js
@@ -1,33 +1,60 @@
-import React, { Component, Suspense, lazy } from 'react';
+import React, { Component } from 'react';
 import {
-  BrowserRouter as Router,
   Route,
   Switch,
   Link
 } from 'react-router-dom'
 
 //const Hello = lazy(() => import('./routes/Hello'));
-import Hello from './routes/Hello';
-import UserList from './routes/Userlist';
-import Forms from './routes/Forms';
+
+// Static loading component
+// import Hello from './routes/Hello';
+// import UserList from './routes/Userlist';
+// import Forms from './routes/Forms';
+// import NotFound from './routes/NotFound';
+
+// Lazy loading component
+import asyncComponent from './components/AsyncComponent';
+const asyncHello = asyncComponent(() => import("./routes/Hello"));
+const asyncUserList = asyncComponent(() => import('./routes/Userlist'));
+const asyncForms = asyncComponent(() => import('./routes/Forms'));
+const asyncNotFound = asyncComponent(() => import('./routes/NotFound'));
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log("-App-");
+  }
+
+
   render() {
     return (
       <div>
         <h1>Home Page</h1>
         <ul>
+          <li><Link to="/">Back To Home Page</Link></li>
           <li><Link to="/hello">Hello</Link> </li>
           <li><Link to="/list"> List page </Link> </li>
-          <li><Link to="/form-demo">Form demo page</Link></li>
+          <li><Link to="/list/form-demo">Form demo page</Link></li>
         </ul>
 
         <Switch>
           {/* <Suspense fallback={<div>Loading....</div>}> */}
-          {/* <Route exact path="/" component={Hello} /> */}
-          <Route path='/form-demo' component={Forms} />
-          <Route path="/hello" component={Hello} />
-          <Route path="/list" component={UserList} />
+
+          {/* Lazy loading component */}
+          <Route path="/hello" component={asyncHello} />
+          <Route exact path="/list" component={asyncUserList} />
+          <Route exact path='/list/form-demo' component={asyncForms} />
+          <Route component={asyncNotFound}></Route>
+
+          {/* Static loading component */}
+          {/* <Route path="/hello" component={Hello} />
+          <Route exact path="/list" component={UserList} />
+          <Route exact path='/list/form-demo' component={Forms} />
+          <Route component={NotFound}></Route> */}
+
+
+          {/* The exact param disables the partial matching for a route and makes sure that it only returns the route if the path is an EXACT match to the current url. */}
           {/* </Suspense> */}
         </Switch>
       </div>
